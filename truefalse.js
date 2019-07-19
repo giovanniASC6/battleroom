2f89a00aa897ea7ea5432d375c0fc4b780138410
giovanniASC6 <giovanni.owens@gmail.com>
2019-07-19 11:02:13 -0400

    battleroom

---
+++ truefalse.js
@@ -0,0 +1,14 @@
+const false1 = Boolean(0 === false);
+
+console.log(false1);
+
+const false2 = Boolean("" === false);
+
+console.log(false2);
+
+console.log(parseInt(null, 24));
+
+// Is that normal, or even true?
+// Indicate if you agree with all, two, one, or none.
+// Answer here:
+
